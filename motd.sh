f5e1a28040d3181823b076af83134a8c4288db3b
Aaron (wutno) <aaron@installgentoo.net>
2014-04-10 20:55:32 -0400

    MOTD script for a DD-WRT router

---
+++ motd.sh
@@ -0,0 +1,19 @@
+let upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
+let secs=$((${upSeconds}%60))
+let mins=$((${upSeconds}/60%60))
+let hours=$((${upSeconds}/3600%24))
+let days=$((${upSeconds}/86400))
+UPTIME=`printf "%d days, %02d:%02d:%02d" "$days" "$hours" "$mins" "$secs"`
+
+echo "
+   .~~.   .~~.    `date +"%A, %e %B %Y, %r"`
+  '. \ ' ' / .'   `uname -srmo`
+   .~ .~~~..~.
+  : .~.'~'.~. :   Uptime.............: ${UPTIME}
+ ~ (   ) (   ) ~  Memory.............: `cat /proc/meminfo | grep MemFree | awk {'print $2'}`kB (Free) / `cat /proc/meminfo | grep MemTotal | awk {'print $2'}`kB (Total)
+( : '~'.~.'~' : ) Load Averages......: $(uptime | grep -Eo 'load.*' | awk '{ print $3,$4,$5 }') (1, 5, 15 min)
+ ~ .~ (   ) ~. ~  Running Processes..: `ps | wc -l | tr -d " "`
+  (  : '~' :  )   Public IP Address..: `wget -q -O - http://icanhazip.com/ | tail`
+   '~ .~~~. ~'    Radio Temperature..: 2.4GHz: $((`wl -i eth1 phy_tempsense|awk {' print $1 '}`/2+20))C / 5GHz: $((`wl -i eth2 phy_tempsense|awk {' print $1 '}`/2+20))C
+       '~'
+"
