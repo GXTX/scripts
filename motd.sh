19392fb2b47f55bd337ade88e676bc9ecb6ce96d
wutno <aaron@installgentoo.net>
2016-02-27 03:56:53 -0500

    credits and some changes

--- motd.sh
+++ motd.sh
@@ -1,3 +1,5 @@
+#adapted from https://www.raspberrypi.org/forums/viewtopic.php?t=23440
+#tested on a R7000 running dd-wrt
 let upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
 let secs=$((${upSeconds}%60))
 let mins=$((${upSeconds}/60%60))
@@ -10,10 +12,10 @@ echo "
   '. \ ' ' / .'   `uname -srmo`
    .~ .~~~..~.
   : .~.'~'.~. :   Uptime.............: ${UPTIME}
- ~ (   ) (   ) ~  Memory.............: `cat /proc/meminfo | grep MemFree | awk {'print $2'}`kB (Free) / `cat /proc/meminfo | grep MemTotal | awk {'print $2'}`kB (Total)
+ ~ (   ) (   ) ~  Memory.............: $((`cat /proc/meminfo | grep MemFree |awk {' print $2 '}`/1000))MB (Free) | $((`cat /proc/meminfo | grep MemTotal |awk {' print $2 '}`/1000))MB (Total)
 ( : '~'.~.'~' : ) Load Averages......: $(uptime | grep -Eo 'load.*' | awk '{ print $3,$4,$5 }') (1, 5, 15 min)
  ~ .~ (   ) ~. ~  Running Processes..: `ps | wc -l | tr -d " "`
-  (  : '~' :  )   Public IP Address..: `wget -q -O - http://icanhazip.com/ | tail`
-   '~ .~~~. ~'    Temperatures.......: CPU: $((`cat /proc/dmu/temperature|awk {' print $1 '}`/10)) | 2.4GHz: $((`wl -i eth1 phy_tempsense|awk {' print $1 '}`/2+20))C / 5GHz: $((`wl -i eth2 phy_tempsense|awk {' print $1 '}`/2+20))C
+  (  : '~' :  )   IP Addresses.......: LAN: `ifconfig br0 | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1` | WAN: `ifconfig vlan2 | grep "inet addr" | cut -d ":" -f 2 | cut -d " " -f 1`
+   '~ .~~~. ~'    Temperatures.......: CPU: $((`cat /proc/dmu/temperature|awk {' print $1 '}`/10))C | 2.4GHz: $((`wl -i eth1 phy_tempsense|awk {' print $1 '}`/2+20))C / 5GHz: $((`wl -i eth2 phy_tempsense|awk {' print $1 '}`/2+20))C
        '~'
 "
