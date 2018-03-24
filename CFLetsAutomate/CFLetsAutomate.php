3ac3e3351af058856a268a0f9a534210c13a7218
wutno <aaron@installgentoo.net>
2018-03-24 16:21:26 +0000

    redo cfletsencrypt

--- CFLetsAutomate/CFLetsAutomate.php
+++ CFLetsAutomate/CFLetsAutomate.php
@@ -12,53 +12,75 @@ class CFLetsAutomate
     private $zone;
     private $dns;
     private $zoneID;
-    private $txtRecord;
-    public  $domain;
+    public $txtRecords;
+    public $domain;
+    public $domains;
 
-    public function __construct(array $info)
+    public function __construct(array $config)
     {
-        if(preg_match('(\*+)', $info['domain'])) //is the input domain a wildcard?
-            $this->domain = substr($info['domain'], 2);
-
-        $this->adapter = new Cloudflare\API\Adapter\Guzzle(new Cloudflare\API\Auth\APIKey($info['apiEmail'], $info['apiKey']));
-        $this->dns     = new Cloudflare\API\Endpoints\DNS($this->adapter);
-        $this->zone    = new Cloudflare\API\Endpoints\Zones($this->adapter);
-        $this->zoneID  = $this->zone->getZoneID($this->domain);
-
-        foreach ($this->dns->listRecords($this->zoneID)->result as $record) {
-            if ($record->type == 'TXT' && $record->name == '_acme-challenge.' . $this->domain) {
-                print "_acme-challenge.{$this->domain} exists in DNS.\n";
-                $this->txtRecord = $record;
-                break; //stop at the first result, this is dumb
-            }
-        }
+        $this->domain = $config['basename'];
+        $this->domains = $config['domains'];
+        $this->adapter = new Cloudflare\API\Adapter\Guzzle(new Cloudflare\API\Auth\APIKey($config['apiEmail'], $config['apiKey']));
+        $this->dns = new Cloudflare\API\Endpoints\DNS($this->adapter);
+        $this->zone = new Cloudflare\API\Endpoints\Zones($this->adapter);
+        $this->zoneID = $this->zone->getZoneID($this->domain);
     }
 
     //return true if there is _acme-challenge in dns
+    //this is broken.. check remote for retrun
     public function check()
     {
+        foreach ($this->dns->listRecords($this->zoneID, 'TXT')->result as $record) {
+            if(strpos($record->name, 'acme-challenge')) {
+                $this->txtRecords[] = [
+                    'id' => $record->id,
+                    'name' => $record->name,
+                    'content' => $record->content,
+                ];
+                print $record->name." exists in DNS.\n";
+            }
+        }
+
         if(!empty($this->txtRecord))
             return true;
         return false;
     }
 
-    //this is a dumb function, that just willy nilly deletes any txt entries with _acme-challenge.domain
+    //deletes every dns entry in $this->txtRecords
     public function delete()
     {
-        if($this->dns->deleteRecord($this->zoneID, $this->txtRecord->id)){
-            print "Deleted _acme-challenge.{$this->domain} TXT record.\n";
+        foreach ($this->txtRecords as $record) {
+            if ($this->dns->deleteRecord($this->zoneID, $record['id'])) {
+                print "Successfully deleted {$record['name']} DNS entry. :)\n";
+            } else {
+                print "Problem deleting {$record['name']}. :(\n";
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function install($identifier, $DNSDigest)
+    {
+        if ($this->dns->addRecord(
+            $this->zoneID,
+            'TXT',
+            '_acme-challenge.'.$identifier,
+            $DNSDigest,
+            120,
+            false
+        )) {
+            print "Created _acme-challenge.{$identifier} with \"{$DNSDigest}\".\n";
             return true;
         }
         return false;
     }
 
-    public function install($dnsContent)
+    public function remoteCheck($identifier, $DNSDigest)
     {
-         if($this->dns->addRecord($this->zoneID, 'TXT', '_acme-challenge.' . $this->domain, $dnsContent, 120, false)){
-            print "Created _acme-challenge.{$this->domain} : {$dnsContent}\n";
-            print "You should wait a few minutes before running finalize.\n";
+        if(!empty($this->dns->listRecords($this->zoneID, 'TXT', '_acme-challenge.'.$identifier, $DNSDigest)->result))
             return true;
-         }
-         return false;
+        return false;
     }
+
 }
--- CFLetsAutomate/program.php
+++ CFLetsAutomate/program.php
@@ -1,75 +1,62 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Developer
- * Date: 3/16/2018
- * Time: 8:20 AM
+
+/*
+ * NOTES:
+ *
+ * 1: We're checking if DNS has the required keys locally with LEFunctions::checkDNSChallenge()
+ *    it should never pass unless it's true, but when we run LEOrder::verifyPendingOrderAuthorization()
+ *    it calls LEFunctions::checkDNSChallenge() and fails, at what i've found to be random so we wait..
+ *    i've found that 3.5 minutes to be about long enough for it to catch it every time and not make
+ *    Let's Encrypts API start spitting out 500 responses when we go to verify.
+ *
+ * 2: LEOrder::verifyPendingOrderAuthorization() expects to have all DNS entries active and does some.. odd
+ *    things when they aren't so we do another for loop after we've verified with LEFunctions::checkDNSChallenge()
  */
 
-require_once ('vendor/autoload.php');
-require_once ('CFLetsAutomate.php');
+require_once('vendor/autoload.php');
+require_once('LEClient/LEClient/LEClient.php');
+require_once('CFLetsAutomate.php');
 
-use stonemax\acme2\Client;
-use stonemax\acme2\constants\CommonConstant;
-
-$helpText = "This program allows you to automate DNS entries for use with Let's Encrypt DNS-01.
-You must set-up the program with your CloudFlare API key & email.
-The program assumes you are trying to make wildcard certificates with Let's Encrypt, anything else hasn't been tested.
-Usage: php program.php (order, finalize, renew) (domain)
-Example: php program.php order *.example.com\n";
-
-$actions = ['order', 'finalize', 'renew'];
-
-$info = [
-    'apiKey' => '', //CloudFlare API key
-    'apiEmail' => '', //CloudFlare API email
-    'letsEncryptEmail' => [''], //this is used for notifications from let's encrypt
-    'storagePath' => '/home/wutno/somefolder/data/' //where the heck is everything going?
+$config = [
+    'apiKey'    => '', //CloudFlare API key
+    'apiEmail'  => '', //CloudFlare API email
+    'LEEmail'   => ['webmaster@example.com'], //this is used for notifications from let's encrypt
+    'basename'  => 'example.com',
+    'domains'   => ['example.com', '*.example.com'],
+    'keyType'   => 'ec-384', //rsa-4096
+    'sleepTime' => 210 //NOTE 1
 ];
 
-if($argc != 3 || empty($argv[1]) || empty($argv[2]))
-    die($helpText);
-else
-    $info += [
-        'action' => $argv[1],
-        'domain' => $argv[2]
-    ];
+$cfHandler = new CFLetsAutomate($config);
+$leHandler = new LEClient($config['LEEmail'], LEClient::LE_PRODUCTION, LECLient::LOG_STATUS);
 
-if(in_array($info['action'], $actions)) {
-    $cfHandle = new CFLetsAutomate($info);
-    $acme2Handle = new Client($info['letsEncryptEmail'], $info['storagePath'], true); //set false for live certs
-} else {
-    die($helpText);
-}
+$order = $leHandler->getOrCreateOrder($config['basename'], $config['domains'], 'ec-384');
 
-if($info['action'] == 'order' || $info['action'] == 'finalize') {
-    $order = $acme2Handle->getOrder([CommonConstant::CHALLENGE_TYPE_DNS => [$info['domain']]], CommonConstant::KEY_PAIR_TYPE_EC);
-} else if($info['action'] == 'renew') {
-    $order = $acme2Handle->getOrder([CommonConstant::CHALLENGE_TYPE_DNS => [$info['domain']]], CommonConstant::KEY_PAIR_TYPE_EC, true);
-    $info['action'] = 'order';
-}
+if(!$order->allAuthorizationsValid()) {
+    $pending = $order->getPendingAuthorizations(LEOrder::CHALLENGE_TYPE_DNS);
+    if (!empty($pending)) {
+        foreach ($pending as $challenge) {
+            if(!$cfHandler->remoteCheck($challenge['identifier'], $challenge['DNSDigest']))
+                $cfHandler->install($challenge['identifier'], $challenge['DNSDigest']);
 
-$challengeList = $order->getPendingChallengeList();
-
-if($info['action'] == 'order') {
-    if ($cfHandle->check())
-        $cfHandle->delete();
-
-    foreach ($challengeList as $challenge) {
-        $challengeType = $challenge->getType(); // http-01 or dns-01
-        $credential = $challenge->getCredential();
-
-        if($challengeType == CommonConstant::CHALLENGE_TYPE_DNS) {
-            $cfHandle->install($credential['dnsContent']);
+            print "Checking for _acme-challenge.{$challenge['identifier']} with \"{$challenge['DNSDigest']}\".\n";
+            while(!LEFunctions::checkDNSChallenge($challenge['identifier'], $challenge['DNSDigest'])){ //NOTE 1
+                print "Not found, waiting 10 seconds and trying again.\n";
+                sleep(10);
+            }
+            print "Found!\n";
+        }
+        print "Waiting " . ($config['sleepTime']/60) . " minutes before we attempt to verify with Let's Encrypt.\n";
+        sleep($config['sleepTime']);
+        foreach($pending as $challenge){ //NOTE 2
+            $order->verifyPendingOrderAuthorization($challenge['identifier'], LEOrder::CHALLENGE_TYPE_DNS);
         }
     }
-} else if($info['action'] == 'finalize') {
-    foreach($challengeList as $challenge){
-        print "We're going to attempt to verify.\n";
-        $challenge->verify();
-        print "Successfully verified with Let's Encrypt!\n";
-    }
-    $certificateInfo = $order->getCertificateFile();
-    vprintf("\nPrivate Key: %-20s\nPublic Key : %-20s\nCertificate: %-20s\nFull Chain : %-20s\n\n", $certificateInfo);
-    $cfHandle->delete();
+}
+
+if($order->allAuthorizationsValid()) {
+    if(!$order->isFinalized()) { $order->finalizeOrder(); }
+    if($order->isFinalized()) { $order->getCertificate(); }
+    $cfHandler->check();
+    if(!empty($cfHandler->txtRecords)) { $cfHandler->delete(); }
 }
